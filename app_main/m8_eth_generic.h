6235f44ca089f71824af1d4f562b0d7e8d6a8b98
Ionut <johnnyalexa@yahoo.com>
2014-04-16 22:53:00 +0300

    Initial commit!

---
+++ app_main/m8_eth_generic.h
@@ -0,0 +1,17 @@
+/*
+ * m8_eth_generic.c
+ *
+ * Created: 4/16/2014 10:33:54 PM
+ *  Author: John
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
